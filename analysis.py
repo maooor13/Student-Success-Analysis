7fc2bd21c81d7bcfc85ea8fb326355a53919686c
angelo-tohar <angelo.tohar@gmail.com>
2026-01-20 13:44:22 +0200

    adding best study method function

--- analysis.py
+++ analysis.py
@@ -27,3 +27,25 @@ def age_sleep_quality_correlation(df):
     '''
     return corr
 
+"""
+The following function will sort the most successful learning method by returning a list where 0 index is the best
+scoring method, any method that wont be included in the list have a small data pool (under 5) that cant give accurate info
+"""
+def best_study_method(df):
+    clean_df = df.dropna(subset=['exam_score','study_method']) #clearing empty cells for honest mean and ranking
+    method_size = clean_df.groupby('study_method')['exam_score'].agg(['mean','count'])
+    #grouping, counting and mean calculating
+    reliable_method_size = method_size[(method_size['count'] > 5)] #not be affected by few unordinary cases
+    sorted_method = reliable_method_size.sort_values(by=['mean'], ascending=False) #sorting method where best performing at [0]
+    return sorted_method['mean'].to_dict()
+
+'''main code for previous function: 
+
+ranking_method_dict = best_study_method(df)
+print("ranking learning methods (above 5 subjects) ")
+for i, (method, average) in enumerate(ranking_method_dict.items(), 1):
+    print(f"{i}. {method}: {average:.2f}")
+'''
+
+
+
