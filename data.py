fed0b1217855392be41a709fabd31010fd88a4a2
maooor13 <maooor13@gmail.com>
2026-01-21 18:25:24 +0200

    Add import_data function

--- data.py
+++ data.py
@@ -7,8 +7,11 @@ from numpy.ma.extras import average
 df = pd.read_csv('data/Exam_Score_Prediction.csv') # reading data file
 
 
-def import_data():
-    pass
+def import_data(filepath='data/Exam_Score_Prediction.csv'):
+    '''
+    filepath must be a path to a csv file. It can be absolute or relative.
+    '''
+    return pd.read_csv(filepath)
 
 def clean_data():
     pass
