ac436355f9e52cca38f912db63285a7b5dfc3654
maooor13 <maooor13@gmail.com>
2026-01-21 19:02:48 +0200

    Move clean NaN to clean_data

--- analysis.py
+++ analysis.py
@@ -95,8 +95,8 @@ def best_study_method(df):
     scoring method, any method that wont be included in the list have a small data pool (under 5) that cant give accurate info
     """
     tmp_df = df.copy()
-    clean_df = tmp_df.dropna(subset=['exam_score','study_method']) #clearing empty cells for honest mean and ranking
-    method_size = clean_df.groupby('study_method')['exam_score'].agg(['mean','count'])
+    
+    method_size = tmp_df.groupby('study_method')['exam_score'].agg(['mean','count'])
     #grouping, counting and mean calculating
     reliable_method_size = method_size[(method_size['count'] > 5)] #not be affected by few unordinary cases
     sorted_method = reliable_method_size.sort_values(by=['mean'], ascending=False) #sorting method where best performing at [0]
